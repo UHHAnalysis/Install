5725cf3ba0d98b4c28a490f5ff7af14ae0fb7bf0
tlapsien <tobias.lapsien@desy.de>
2013-10-15 17:06:49 +0200

    return -> exit

--- SFrameInstall.sh
+++ SFrameInstall.sh
@@ -2,12 +2,12 @@
 
 if [ "$#" -ne 2 ] ; then
     echo "Usage: ./SFrameInstall.sh <directory to install sframe> <directory of fastjet lib directory>"
-    return
+    exit 1;
 fi
 
 if [ "${ROOTSYS}" = "" ] ; then
     echo "Please setup ROOT before calling this script"
-    return 
+    exit 1;
 fi
 
 SFRAMEDIR=`readlink -f $1`
@@ -15,28 +15,28 @@ FASTJETDIR=`readlink -f $2`
 
 if [ -z $SFRAMEDIR ]; then
    echo "Error: could not resolve given SFRAME target directory '$1'. Make sure to provide a path for which all components but the last exist, omitting the trailing '/'.";
-   return 1;
+   exit 1
 fi
    
 
 if [[ -e $SFRAMEDIR ]]; then
     echo "Error: directory $SFRAMEDIR already exists."
-    return 1;
+    exit 1;
 fi
 
 if [ ! -e $FASTJETDIR/libfastjet.so ]; then
     echo "Error: fastjet dir does not contain 'libfastjet.so'";
-    return 1;
+    exit 1;
 fi
 
 svn co https://svn.code.sf.net/p/sframe/code/SFrame/tags/SFrame-03-06-11 $SFRAMEDIR
 #if [ "$_" -neq "0" ]; then
 #   echo "svn co failed!";
-#   return 1;
+#   exit 1;
 #fi
 
 
-cd $SFRAMEDIR || { echo "svn co failed!"; return 1; }
+cd $SFRAMEDIR || { echo "svn co failed!"; exit 1; }
 # remove -lpcre from the core to make sframe_main compile:
 sed -i s/-lpcre// core/Makefile
 # create and source fullsetup.sh:
